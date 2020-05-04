909d1da7179b03b4f6c440b7e4fc068be16dc875
wykhuh <wykhuh@users.noreply.github.com>
2020-05-04 14:01:19 -0700

    increase pagination PER_PAGE

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_ORM")
     SECRET_KEY = os.environ.get("APP_SECRET_KEY")
-    PER_PAGE = 5
+    PER_PAGE = 20
 
 
 class DevelopmentConfig(Config):
