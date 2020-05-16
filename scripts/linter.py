86a36791742f9ced0a0dd77d77440a9042b3e239
wykhuh <wykhuh@users.noreply.github.com>
2020-05-16 04:35:32 -0700

    update linter directories

--- scripts/linter.py
+++ scripts/linter.py
@@ -4,10 +4,8 @@ import subprocess
 def lint():
     _exec("black ./app")
     _exec("black ./scripts")
-    _exec("black ./tests")
     _exec("flake8 ./app")
     _exec("flake8 ./scripts")
-    _exec("flake8 ./tests")
 
 
 def _exec(command):
